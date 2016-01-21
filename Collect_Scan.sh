2f84cfd065b3905b6e2284e31ee18f232feb1619
silent-mobius <silent-mobius@users.noreply.github.com>
2016-01-21 19:11:45 +0200

    minor changes
    still not ready - need to finish last 2 functions.

--- Collect_Scan.sh
+++ Collect_Scan.sh
@@ -3,12 +3,23 @@
 #############################################################################
 #Purpose: 
 #       Adjust script running to the scan
-# Created by : br0k3ngl255
+#created by : nr0k3ngl255
 ##############################################################################
 ##Vars_ - _ - _ - _ - _ -_- _ - _ - _ - _ - _ - _ - _ - _ - _ - _ - _ - _ - _ - _ -
 logFile="scan.txt"
 folder="/tmp"
 ##funcs+ + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + +
+verify_tools(){
+	check_tool=$(ls /usr/bin|grep $t > /dev/null ;echo $?)
+	tools=('nmap' 'xprobe2' 'nbtscan')
+	for t in $tools;do
+		if [ check_tool == "0" ];then
+			True
+		else
+			echo "Please install $t"
+		fi
+				done
+	}
 osCheck(){
 xprobe2  $ip |grep "Running OS"|head -1|awk {'print  "OS[" $6 $7 "\t" $8"]"'} >> $folder/$logFile &
 }
@@ -16,20 +27,21 @@ nameCheck(){
 nbtscan -r $ip|tail -1|awk {'print "PC_name[" $2 "]" '} >> $folder/$logFile &
 }
 
-portScan(){
-echo "IP[${ip}]" >> $folder/$logFile # what ip am I running on??
-portScanner_byport.py $ip|grep Open  >> $folder/$logFile 2> /dev/null &
-}
 backupPlan(){
     nmap -O $ip|grep MAC|awk {'print "Manufactorer ["$4 "\t" $5 "\t" $6"]"'} >> $folder/$logFile &
 }
 cleanStat(){
     echo " " > $folder/$logFile
 }
-
+portKnock(){
+	python << EOF
+	from scapy import *
+	
+EOF
+	}
 ##Actions ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
-while getopts "i:f:p:" OPTIONS;do
+while getopts "i:f:p:" OPTIONS;do #use these paramaeters to tun the script
      case ${OPTIONS} in
           i ) interface=$OPTARG;;
           f ) folder=$OPTARG;;
@@ -38,12 +50,17 @@ while getopts "i:f:p:" OPTIONS;do
      esac
 done
 
+
+verify_tools
+
 if [ ! -e /tmp/scan.txt ];then
 touch /tmp/scan.txt
 else 
 	cleanStat
 fi
 
+
 nameCheck
 	osCheck;backupPlan
-		portScan
+	portKnock
+		
