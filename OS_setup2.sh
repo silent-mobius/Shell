acf30886bb5040a1e7b86924e7459134e0e38682
silent-mobius <silent-mobius@users.noreply.github.com>
2016-10-18 14:37:38 -0400

    added variables

--- OS_setup2.sh
+++ OS_setup2.sh
@@ -15,6 +15,8 @@ log="install_log.txt"
 logFile="$logFile/$log"
 PASSWD="1"
 USER="mobius"
+KODENAME="jessie"
+REPONAME="debian"
 declare -a packages=( 'lightdm' 'mate-desktop-environment-extras' 'firmware-realtek' 'firmware-linux' 'firmware-linux-free'
 'firmware-linux-nonfree' 'vlc' 'gparted' 'abiword' 'transmission' 'guake' 'mixxx' 'culmus' 'xfonts-efont-unicode'
 'xfonts-efont-unicode-ib' 'xfonts-intl-european' 'ttf-mscorefonts-installer' 'sqlite' 'sqlite3' 'mysql-client' 'mysql-server'
@@ -117,60 +119,3 @@ pacInstall(){
 		if [ `uname -a|grep kali > /dev/null;echo $?` == "0" ];then
 			updatedb;locate kali |grep png > pics.txt
 				while read line;do rm -rf $line;done  < pics.txt
-		fi
-	  #arainging numbers for editor
-	  echo "set number" >> /etc/vim/vimrc
-		rm -rf /usr/share/kali-defaults/bookmarks.html
-		rm -rf /usr/share/kali-defaults/web
-		rm -rf /usr/share/kali-defaults/localstore.rdf
-	  sed -i -e s/TIMEOUT=5/TIMEOUT=0/ /etc/default/grub
-	  sed -i -e s/GRUB_CMDLINE_LINUX_DEFAULT="quiet"/GRUB_CMDLINE_LINUX_DEFAULT="quiet splash"/ /etc/default/grub;update-grub;update-initramfs -u
-	  if [ -e /etc/gdm3/greeter.gsettings ];then
-				sed -i -e s/kali-dragon.png/ /g   /etc/gdm3/greeter.gsettings
-				sed -i -e s/kali-dragon.png/ /g   /etc/gdm3/greeter.gsettings.dpkg-new
-	  else
-				true
-	  fi
-	  if [ -e /usr/share/gdm/dconf/10-desktop-base-settings ];then
-				sed -i -e s/kali-dragon.png/ /g   /usr/share/gdm/dconf/10-desktop-base-settings
-				sed -i -e s/login-background.png/ /g /usr/share/gdm/dconf/10-desktop-base-settings
-	  else
-				true
-		fi'
-	    }
-
-link_install(){
-declare -a LINKS=(
-'http://download.teamviewer.com/download/teamviewer_amd64.deb'
-'https://geany-vibrant-ink-theme.googlecode.com/files/vibrant_ink_geany_filedefs_20111207.zip'
-'https://download.jetbrains.com/python/pycharm-professional-2016.2.3.tar.gz'
-'https://atom.io/download/deb'
-'http://kdl.cc.ksosoft.com/wps-community/download/a21/wps-office_10.1.0.5672~a21_amd64.deb' 
-)
-for i in ${LINKS[@]}
-	do
-			wget $i &> $logFile $
-	done
-
-
-}
-####
-#Main - _- _- _- _- _- _- _- _- _- _- _- _- _- _- _- _- _- _- _- _- _- _
-####
-while getopts ":i:u:p:" options;do
-	case $options in
-
-		i)	INSTALL_MNGR=$OPTARG;;
-		u)	USER=$OPTARG;;
-		p)	PASSWD=$OPTARG;;
-		*) echo "error";;
-
-	esac
-done
-
-if [ "$EUID" != "0" ];then
-		echo "Please get Root priviledges"
-		help;sleep 2;exit
-else
-		set_working_env; netCheck;
-fi
