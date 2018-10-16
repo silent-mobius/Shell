7d0aacbb3fa43258589df06fe37d520596ece7bf
br0k3ngl255 <alex@vaiolabs.com>
2018-10-16 11:40:19 +0300

    adding cron monitoring script

--- cron_job_monitor.sh
+++ cron_job_monitor.sh
@@ -13,15 +13,66 @@ cron_time_stamp=5
 logfile=cron_job_mon.log
 logfolder=/var/log/cron_mon
 logFile=$logfolder/$logfile
-
-
+alert_color='\033[0;31m'
+suggest_color='\033[1;36m'
+NC='\033[0m' # No Color
 
 #Funcs /\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
+usage(){
+	printf "${alert_color} Incorrect use${NC}\n"
+	cron_job_monitor -j JOB/SERVICE -t TIMESTAMP
+	}
 
 
+help(){
+	printf "${suggest_color} Incorrect use${NC}\n"
+	printf "${suggest_color}cron_job_monitor -j JOB/SERVICE -t TIMESTAMP ${NC}\n"
+	printf ""
+	
+	}
+	
+log_check(){
+	if [ ! -e $logfolder ];then
+		echo 'creating log folder'
+			mkdir -p $logfolder
+			
+			if [ "$?" == "0" ];then
+				true;
+			else
+				echo "something went wrong --> check permissions"
+				exit 1
+			fi
+				
+	fi
+	
+	if [ ! -e $logfile ]
+		echo 'creating log file'
+			touch $logfolder/$logfile
+			
+			if [ "$?" == "0" ];then
+				true;
+			else
+				echo "something went wrong --> check permissions"
+				exit 1
+			fi
+	if [ -e $logFile ];then
+		echo 'log file exists'
+		true;
+	else
+		echo "something went wrong --> check permissions"
+			exit 1
+		
+	fi
+	}
 
+cron_job_setup(){
+	
+	}
 
 
 ###
 #Main - _- _- _- _- _- _- _- _- _- _- _- _- _- _- _- _- _- _- _- _- _- _
 ###
+
+if [ $EUID == "0" ];then
+	echo "Invalid User -- Please get Root Privileges"
