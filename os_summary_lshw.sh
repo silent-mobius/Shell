d141dd698bdee39d5ac245d740266ead3b63a05f
br0k3ngl255 <alex@vaiolabs.com>
2018-11-20 14:23:54 +0200

    pulling stuff

--- os_summary_lshw.sh
+++ os_summary_lshw.sh
@@ -2,12 +2,18 @@
 
 ##################################################################
 #created by     : br0k3ngl255
-#date 		: 22.10.2018
-#purpose	: get constructed data from Linux/Unix systems and write them either stdout or to csv file.
-#ver		: 0.2.15
+#date 			: 22.10.2018
+#purpose		: get constructed data from Linux/Unix systems and 
+#					write them either stdout or to csv file.
+#ver			: 0.2.15
 ##################################################################
-
-
+##################################################################
+#ToDo:
+#	* orginze and filter data geberated by "lshw"
+#	* need to manage data as associative array
+#	* create a function to write data as csv file.
+#	* create a function to formate data as database format
+##################################################################
 clear
 #variables +++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
@@ -32,55 +38,46 @@ stor_msg="List Of Mount Points"
 mod_msg="List of Kernel Modules"
 permission_msg="Please Get Root Access"
 
-
+###########################################################
+: '
+declare -n bridge_arr
+declare -n bus_arr
+declare -n communication_arr
+declare -n display_arr
+declare -n generic_arr
+declare -n memory_arr
+declare -n network_arr
+declare -n processor_arr
+declare -n storage_arr
+declare -n system_arr
+declare -n volume_arr
+'
+###########################################################
 
 #System variables
-MD_version=$(env|grep MD_Ver|awk -F= '{print $2}')
-MD_product=$(env|grep MD_Prod|awk -F= '{print $2}')
-OS=$(cat /etc/*-release|grep ID|awk -F= '{print $2}'|head -n1|sed 's/"//g')
-HostName_arr=($(hostnamectl status))
+md_version=$(env|grep MD_Ver|awk -F= '{print $2}')
+md_product=$(env|grep MD_Prod|awk -F= '{print $2}')
+os=$(cat /etc/*-release|grep ID|awk -F= '{print $2}'|head -n1|sed 's/"//g')
+local_users_arr=$(while IFS= read -r i;do echo $i|awk -F : '{print $1}'; done < /etc/passwd )
+host_name_arr=($(hostnamectl status))
+net_addr_arr=($(ip addr show | grep inet|grep -v inet6|grep -v "127.0.0.1"))
 
 #installedApplications - here is  what's problmatic here
 
-if [ $OS == "centos" ] || [ $OS == "redhat" ] || [ $OS == "fedora" ];then
-	installedAppArray=$(rpm -qa |awk -F. '{print $1}'|uniq|sort)
+if [ $os == "centos" ] || [ $os == "redhat" ] || [ $os == "fedora" ];then
+	installed_App_arr=$(rpm -qa |awk -F. '{print $1}'|uniq|sort)
 else
 		true
 fi
 
-if [ $OS == "debian" ] || [ $OS == "ubuntu" ];then
-	installedAppsArray=$(dpkg -l|awk -F. '{print $1}'|uniq|sort)
+if [ $os == "debian" ] || [ $os == "ubuntu" ];then
+	installed_apps_arr=$(dpkg -l|awk -F. '{print $1}'|uniq|sort)
 else
 		true
 fi
 
 
 #Functios /\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
-
-hw_ls(){
-
-if  $(which lshw);then
-	printf "%s " $cpu_info
-else
-	printf "%s " "need to install lshw"
-
-fi
-}
-
-
-LocalUsers(){
-usersdb="/etc/passwd"
-while IFS=: read -r i
-
-do
-cmd=$(echo $i|awk -F : '{print $3}')
-
-	if [ $cmd -ge 1000 -a $cmd -le 2000 ];then
-		LocalUsersArray+=($i)
-	fi
-done <"$usersdb"
-
-}
 
 hw_ls(){
 
@@ -92,7 +89,13 @@ fi
 
 }
 
-
+filter_func(){
+	#declare -n
+	 arr=$1
+    for key in "${!arr[@]}"; do
+       printf "%s\t%s" "$key ${arr[$key]}"
+    done
+	}
 
 ###
 #Main - _- _- _- _- _- _- _- _- _- _- _- _- _- _- _- _- _- _- _- _- _- _- _- _- _- _- _
@@ -116,29 +119,32 @@ else
 
 #HW list:
 printf "%-20s" "$note_msg : Scanning the System"
+
 bridge_arr=($(lshw -class bridge -quiet $output_var))
 bus_arr=($(lshw -class bus -quiet $output_var))
 communication_arr=($(lshw -class communication -quiet $output_var))
 display_arr=($(lshw -class display -quiet $output_var))
-generic_arr=($(lshw -class generic -quiet$output_var))
-memory_arr=($(lshw -class memory -quiet$output_var))
+generic_arr=($(lshw -class generic -quiet $output_var))
+memory_arr=($(lshw -class memory -quiet $output_var))
 network_arr=($(lshw -class network -quiet $output_var))
 processor_arr=($(lshw -class processor -quiet $output_var))
 storage_arr=($(lshw -class storage -quiet $output_var))
 system_arr=($(lshw -class system -quiet $output_var))
-volume_arr=($(lshw -class volume -quiet$output_var))
+volume_arr=$(lshw -class volume -quiet $output_var)
 
-printf "%s: %s\n" "${bridge_arr[@]}"
-printf "%s: %s\n" "${bus_arr[@]}"
-printf "%s: %s\n" "${communication_arr[@]}"
-printf "%s: %s\n" "${display_arr[@]}"
-printf "%s: %s\n" "${generic_arr[@]}"
-printf "%s: %s\n" "${memory_arr[@]}"
-printf "%s: %s\n" "${network_arr[@]}"
-printf "%s: %s\n" "${processor_arr[@]}"
-printf "%s: %s\n" "${storage_arr[@]}"
-printf "%s: %s\n" "${system_arr[@]}"
-printf "%s: %s\n" "${volume_arr[@]}"
-
+: '
+printf "%s" "${bridge_arr[@]}"
+printf "\n%s" "${bus_arr[@]}"
+printf "\n%s" "${communication_arr[@]}"
+printf "\n%s" "${display_arr[@]}"
+printf "\n%s" "${generic_arr[@]}"
+printf "\n%s" "${memory_arr[@]}"
+printf "\n%s" "${network_arr[@]}"
+printf "\n%s" "${processor_arr[@]}"
+printf "\n%s" "${storage_arr[@]}"
+printf "\n%s" "${system_arr[@]}"
+printf "${volume_arr[@]}"
+'
+filter_func volume_arr
 
 fi
