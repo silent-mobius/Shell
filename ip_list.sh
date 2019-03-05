e8d9b2011aae1044eb94ab9ae49c8fd76cca6d89
br0k3ngl255 <alex@vaiolabs.com>
2019-03-05 16:08:46 +0200

    adding support for ip function

--- ip_list.sh
+++ ip_list.sh
@@ -4,13 +4,15 @@
 #created by	: br0k3ngl255
 #purpose	: print table of network interfaces with ip and mac addrs
 #date		: 27.02.2019
-#version	: 0.2.19
+#version	: 0.4.32
 ########################################################################
 # task ahead:
 	#create a script that will print all the interfaces, with their IP 
 	#MAC addresses in a pretty table.
 
 ###variables ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+tool_status=0
+
 #messages
 msg_parameter_missing=" you have not provided one of the paramenters :"
 msg_unvalid_user="You are not allowed to recieve this type of information"
@@ -33,14 +35,17 @@ line="================================================================="
 
 check_bin(){
 	if [ -e $(which ifconfig) ];then
-		true
+			tool_status=0
+		return $tool_status
 	else
 		printf "%s " $msg_file_missing
+				tool_status=1
+			return $tool_status
 		exit 1
 	fi
 	}
 	
-table_print(){
+ifconfig_table_print(){
 
 	local format=" %-16s %-16s %12s \n"
 	
@@ -67,6 +72,34 @@ table_print(){
 		done
 	printf "%s\n" "$line"
 	}
+	
+ip_table_print(){
+
+	local format=" %-16s %-16s %12s \n"
+	
+	INETS=$(ip l|awk -F": " '{print $2}')
+
+	
+	printf "%s\n" "$line"
+		printf "$format"  "INET" "IP" "MAC"
+	printf "%s\n" "$line"
+	
+	for inet in  $INETS
+		do
+			IP=$(ip addr show $inet|grep -v inet6|grep inet|awk '{print $2}')
+			MAC=$(ip addr show $inet|grep ether|awk '{print $2}')
+			if [[ $IP == "" ]];then
+				IP="#### NONE ####"
+			fi
+			if [[ $MAC == "" ]];then
+				MAC="#### NONE ####"
+			fi
+			
+			printf "$format" "$inet" "$IP" "$MAC"
+				
+		done
+	printf "%s\n" "$line"
+	}
 ####
 #Main - _- _- _- _- _- _- _- _- _- _- _- _- _- _- _- _- _- _- _- _- _- _
 ####
@@ -77,5 +110,6 @@ if [ $valid_user != 0 ];then
 	exit 1
 else	
 	check_bin
-		table_print
+	#ifconfig_table_print
+	ip_table_print
 fi
