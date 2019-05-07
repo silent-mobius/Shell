b231884b4e854604232205afd59b180e195af5d6
silent-mobius <alex@vaiolabs.com>
2019-05-07 22:32:06 +0300

    cleaning the script

--- gen_template_script.sh
+++ gen_template_script.sh
@@ -5,7 +5,7 @@
 # purpose	 : create template generation that will read variables
 # from stdin and will generate script 
 # date		 : 25/12/2018
-# version	 : 0.0.1
+# version	 : 0.2.3
 ##############################################################################
 
 ##Vars :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
@@ -20,7 +20,7 @@ line="########################################################################"
 #conf vars
 Time=1
 shabang="/usr/bin/env"
-script_name="template_script"
+script_name=$1
 extension="sh"
 comment="#"
 Date="Date: $(date +%d/%m/%y)"
@@ -33,6 +33,9 @@ Auth="Create by : "
 #Main - _- _- _- _- _- _- _- _- _- _- _- _- _- _- _- _- _- _- _- _- _- _- _- _
 ####
 
+if [ $script_name == "" ];then
+	read -p "what shall we call the script? ==>>" script_name
+fi
 printf "%s \n" $line
 printf "%s \n" $msg_start
 printf "%s \n" $line
@@ -40,13 +43,13 @@ printf "%s \n" $line
 	sleep $Time
 
 printf "%s \n" $msg_create
-printf "%s \n" $shabang > "$script_name\.$extension"
-printf "%s \n" $line >> "$script_name\.$extension"
-printf "%s \n" "$comment $Date" >> "$script_name\.$extension"
-printf "%s \n" "$comment $Purp" >> "$script_name\.$extension"
-printf "%s \n" "$comment $Ver" >> "$script_name\.$extension"
-printf "%s \n" "$comment $Auth" >> "$script_name\.$extension"
-printf "%s \n" $line >> "$script_name\.$extension"
+printf "%s \n" $shabang > "$script_name.$extension"
+printf "%s \n" $line >> "$script_name.$extension"
+printf "%s \n" "$comment $Date" >> "$script_name.$extension"
+printf "%s \n" "$comment $Purp" >> "$script_name.$extension"
+printf "%s \n" "$comment $Ver" >> "$script_name.$extension"
+printf "%s \n" "$comment $Auth" >> "$script_name.$extension"
+printf "%s \n" $line >> "$script_name.$extension"
 
 	sleep $Time
 
