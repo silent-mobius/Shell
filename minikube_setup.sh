022e53d4cea28a2e9184d9863c7c564f9fab48d5
silent-mobius <alex@vaiolabs.com>
2019-05-07 22:26:46 +0300

    adding minikube setup script

---
+++ minikube_setup.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+#####################################################################################
+#created by: smobius
+#purpose : genersal script for installing k8s
+#date	: 07.05.2019
+#version: 1.0.0
+#####################################################################################
+
+###Vars ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+#messages
+msg_error="Something went wrong - try running in debug mode"
+msg_note="Notification "
+msg_start="Start OF Script"
+msg_end="End Of Script"
+msg_permission="Please Get Root Access"
+msg_unsupported="This OS is NOT supported"
+msg_start_install="starting installing packages and group packages"
+msg_add_repo="adding repo"
+msg_installer_set="finished setting up installer"
+#misc
+line="#############################################################################"
+
+
+###Funcs /\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\
+
+
+
+
+
+
+####
+#Main -  _-  _-  _-  _-  _-  _-  _-  _-  _-  _-  _-  _-  _-  _-  _-  _-  _-  _-  _-  _
+####
