5b3c110a5b2ba181c2a153137f496c774d81e377
silent-mobius <alex@vaiolabs.com>
2019-06-13 22:50:46 +0300

    going back to testing

--- rpm_provision.sh
+++ rpm_provision.sh
@@ -4,7 +4,7 @@
 # created by : br0k3ngl255
 # purpose	 : to provision rpm based laptops for development
 # date		 :  14/12/2018
-# version	 : 0.8.5
+# version	 : 0.8.7
 ##############################################################################
 #set -x
 : 'TODO
