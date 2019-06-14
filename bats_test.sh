851a7d2b0f22ea73d9c4a05525e9a7cdb0a91c06
Silent Mobius <que@vaio.labs>
2019-06-14 18:37:10 +0300

    adding bats init file

--- bats_test.sh
+++ bats_test.sh
@@ -4,6 +4,37 @@
 # created by : br0k3ngl255
 # purpose        : to provision rpm based laptops for development
 # date           :  14/12/2018
-# version        : 0.8.5
+# version        : 0.0.7
 ##############################################################################
 
+
+###Variables:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+
+
+###Functions /\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
+
+
+try(){
+    [[ $- = *e* ]]; SAVED_OPT_E=$?
+    set +e
+}
+
+throw(){
+    exit $1
+}
+
+except(){
+    export ex_code=$?
+    (( $SAVED_OPT_E )) && set +e
+    return $ex_code
+}
+
+throwErrors(){
+    set -e
+}
+
+
+#####
+#Main
+#####
