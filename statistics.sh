26ca543785a838545916392ef2ba7553d080ca4c
Alex M. Schapelle <fifo@fifolabs.fifodomain>
2019-09-12 00:13:15 +0300

    adding new shell script

---
+++ statistics.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+ #set -xe
+###############################################################################
+#created by: pushtakio
+#purpose: provide stats for CPU,MEM,STRGE
+#date: 09/09/2019
+#ver: 1.1.14
+###############################################################################
+#
+####Variables   :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+#msgs:
+msg_root="Please DO NOT run script as root"
+_time=5
+_date=$(date +%H:%M:%S_%d/%m/%Y)
+_end=$((SECONDS+360))
+_cpu=$(top -bn1 | grep load | awk '{printf "%.2f%%\t\t\n", $(NF-2)}')
+_mem=$(free -m | awk 'NR==2{printf "%.2f%%\t\t", $3*100/$2 }')
+_disk=$(df -h | awk '$NF=="/"{printf "%s\t\t", $5}')
+log_folder="/home/prod/.pm2/logs/"
+
+####Functions    /\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
+
+deco(){
+  printf "{%s:%s,%s:%s,%s:%s}\n" "$@"
+}
+
+test_log_folder(){
+  if [[ -d $log_folder ]];then
+    true
+  else
+    mkdir -p $log_folder
+fi
+}
+
+#####
+#Main - _- _- _- _- _- _- _- _- _- _- _- _- _- _- _- _- _- _- _- _- _- _- _- _
+#####
+
+if [[ $EUID == 0 ]];then
+  deco $msg_root
+else
+test_log_folder
+while [[ $SECONDS < $_end ]]
+  do
+    deco cpu $_cpu mem $_mem  hdd $_disk >> $log_folder/stat.log
+    sleep $_time
+  done
+fi
+
