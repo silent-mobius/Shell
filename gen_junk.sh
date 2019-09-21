f2186975ee0dfc5e36617c28aa2e4156e27e9eed
pushtakio <alex@vaiolabs.com>
2019-09-21 23:20:12 +0300

    adding initial junk script

---
+++ gen_junk.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+#set -ex
+################################################################################
+# created by pushtakio
+# purpose: create junk data for showing how to use tar and alike tools.
+# date: 21/09/2019
+# vers: 1.0.0
+###############################################################################
+
+####Vars  :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+#messages
+msg_root="Please do not use ROOT User for this script"
+msg_permissions="You do not have sufficiant ppermissins to use this script in destination folder"
+
+#misc
+_time=1.5
+line="#########################################################################"
+####Functions /\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
+deco(){
+  pre=$line
+  post=$line
+
+  print "\n$pre\n#%s \n$post" "$@"
+- _
+}
+
+
+
+#####
+#Main - _- _- _- _- _- _- _- _- _- _- _- _- _- _- _- _- _- _- _- _- _- _- _- _-
+#####
+
+
+if [[ $EUID == 0 ]];then
+    deco $msg_root
+else
+  true
+fi
