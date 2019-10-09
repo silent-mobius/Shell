92ba2e1efac5da086d186505a14f29ec61cab60c
pushtakio <alex@vaiolabs.com>
2019-10-09 04:36:09 +0300

    affing comments

--- bash_matrix.sh
+++ bash_matrix.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
+#set -xe
+##############################################################################
+# edited by: Pushtakio --> created by ### http://bruxy.regnet.cz/web/linux ###
+#purpose: matrix printout
+#date: 08/10/2019
+#version : 1.1.0
+##############################################################################
 echo -e "\033[2J\033[?25l"
 R=`tput lines` C=`tput cols`
  : $[R--]
   while true
     do ( e=echo\ -e s=sleep j=$[RANDOM%C] d=$[RANDOM%R];
       for i in `eval $e {1..$R}`;
-        do c=`printf '\\\\0%o' $[RANDOM%57+33]` ### http://bruxy.regnet.cz/web/linux ###
+        do c=`printf '\\\\0%o' $[RANDOM%57+33]`
           $e "\033[$[i-1];${j}H\033[32m$c\033[$i;${j}H\033[37m"$c;
            $s 0.01;
            if [ $i -ge $d ];then
