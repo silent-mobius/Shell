f361c96e9c3f659c69c32427093d265c0c130c17
silent-mobius <alex@vaiolabs.com>
2019-11-11 05:31:47 +0200

    adding server up script

---
+++ server_up_notify.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+#set -xe
+##################################################################
+# Created by: Pushtakio
+# Purpose: get details of server and notify whe it is up.
+# Date: 11/11/2019
+# Version: 1.0.0
+##################################################################
